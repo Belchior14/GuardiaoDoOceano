03ebd1cdc7e0c00699951c6044617466152e9f95
Belchior <belchior.fontao@gmail.com>
2022-07-28 16:27:21 +0100

    changes to deploy on github

--- src/Components/Sponsors/index.js
+++ src/Components/Sponsors/index.js
@@ -1,23 +1,33 @@
-import "./sponsors.css"
-import agriloja from "./sponsorsImage/agriloja.png"
-import gamaUno from "./sponsorsImage/gamaUno.png"
-import qbq from "./sponsorsImage/qbq.png"
-import cmMafra from "./sponsorsImage/cmMafra.jpg"
-import cmTorres from "./sponsorsImage/cmTorresVedras.png"
+import "./sponsors.css";
+import agriloja from "./sponsorsImage/agriloja.png";
+import gamaUno from "./sponsorsImage/gamaUno.png";
+import qbq from "./sponsorsImage/qbq.png";
+import cmMafra from "./sponsorsImage/cmMafra.jpg";
+import cmTorres from "./sponsorsImage/cmTorresVedras.png";
 
-export function Sponsors () {
-    return(
-        <div className="mainSponsors">
-            <div className="sponsorsName">
-                <h2>Apoios:</h2>
-            </div>
-            <div className="theSponsors">
-            <img src={agriloja} alt="agriloja" />
-            <img src={gamaUno} alt="gamaUno" />
-            <img src={qbq} alt="qbq" />
-            <img src={cmTorres} alt="cmTorres" />
-            <img src={cmMafra} alt="cMafra" />
-            </div>
-        </div>
-    )
+export function Sponsors() {
+  return (
+    <div className="mainSponsors">
+      <div className="sponsorsName">
+        <h2>Apoios:</h2>
+      </div>
+      <div className="theSponsors">
+        <a href="https://agriloja.pt/" target="_blank" rel="noreferrer">
+          <img src={agriloja} alt="agriloja" />
+        </a>
+        <a href="https://www.gamauno.pt/" target="_blank" rel="noreferrer">
+          <img src={gamaUno} alt="gamaUno" />
+        </a>
+        <a href="https://qbq.pt/" target="_blank" rel="noreferrer">
+          <img src={qbq} alt="qbq" />
+        </a>
+        <a href="https://www.cm-tvedras.pt/" target="_blank" rel="noreferrer">
+          <img src={cmTorres} alt="cmTorres" />
+        </a>
+        <a href="https://www.cm-mafra.pt/" target="_blank" rel="noreferrer">
+          <img src={cmMafra} alt="cMafra" />
+        </a>
+      </div>
+    </div>
+  );
 }
